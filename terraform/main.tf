d8c64f7c4486667bdea2547684c47d71e4bcea2b
Kmil027 <camunoz960@gmail.com>
2026-02-13 18:58:48 -0500

    2

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,27 +12,35 @@ provider "proxmox" {
   pm_api_token_id = var.proxmox_api_token_id
   pm_api_token_secret = var.proxmox_api_token_secret
   pm_tls_insecure = true
-  pm_log_enable = true
-  pm_log_file   = "terraform-plugin-proxmox.log"
 }
 
 resource "proxmox_vm_qemu" "nodos_k3s" {
   count       = 4
   name        = "nodo${count.index + 1}"
   target_node = "proxmox-lab"
-  clone       = "ubuntu-2404-template" 
-  vmid        = 300 + count.index      
+  clone       = "ubuntu-2404-template"
+  vmid        = 300 + count.index
   
+  # Forzar clonación completa y arranque automático
+  full_clone = true
+  onboot     = true
+  agent      = 1
+
+  # Recursos
   cores   = 2
   sockets = 1
   memory  = 2048
   cpu     = "host"
-  agent   = 1 
 
-  os_type   = "cloud-init"
-  ipconfig0 = "ip=192.168.10.${223 + count.index}/24,gw=192.168.10.1"
-  ciuser      = "root"          
-  cipassword  = "Camilo08" 
+  # Configuración Cloud-Init (Evita el error de conversión de datos)
+  os_type    = "cloud-init"
+  ciuser     = "root"
+  cipassword = "Camilo08"
+  
+  # Estos parámetros ayudan a que el plugin no reciba valores nulos de la API
+  nameserver   = "8.8.8.8"
+  searchdomain = "local.lan"
+  ipconfig0    = "ip=192.168.10.${223 + count.index}/24,gw=192.168.10.1"
   
   sshkeys = <<EOF
   ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILmxDrS6ZLy/HxPdP5mN135maZcrWyGeF2NpfQbiB4IC
@@ -43,9 +51,6 @@ resource "proxmox_vm_qemu" "nodos_k3s" {
     type    = "scsi"
     storage = "local"
   }
-
-  full_clone = true
-  onboot     = true
 }
 
 variable "proxmox_api_token_id" {}
