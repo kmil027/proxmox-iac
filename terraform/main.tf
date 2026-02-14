d96b9bbd43358b9788f814eb7d0b7352d8a6c79e
Kmil027 <camunoz960@gmail.com>
2026-02-13 19:01:34 -0500

    3

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     proxmox = {
       source  = "telmate/proxmox"
-      version = "2.9.11"
+      # Cambiamos de 2.9.11 a una versión que corrige el crash de Cloud-Init
+      version = "3.0.1-rc6" 
     }
   }
 }
@@ -12,6 +13,9 @@ provider "proxmox" {
   pm_api_token_id = var.proxmox_api_token_id
   pm_api_token_secret = var.proxmox_api_token_secret
   pm_tls_insecure = true
+  
+  # Aumentamos el timeout para evitar que el plugin "no responda" durante el clonado
+  pm_timeout = 600 
 }
 
 resource "proxmox_vm_qemu" "nodos_k3s" {
@@ -21,25 +25,21 @@ resource "proxmox_vm_qemu" "nodos_k3s" {
   clone       = "ubuntu-2404-template"
   vmid        = 300 + count.index
   
-  # Forzar clonación completa y arranque automático
   full_clone = true
   onboot     = true
   agent      = 1
 
-  # Recursos
   cores   = 2
   sockets = 1
   memory  = 2048
   cpu     = "host"
 
-  # Configuración Cloud-Init (Evita el error de conversión de datos)
   os_type    = "cloud-init"
   ciuser     = "root"
   cipassword = "Camilo08"
   
-  # Estos parámetros ayudan a que el plugin no reciba valores nulos de la API
+  # Obligatorio para evitar que la API devuelva valores vacíos que rompan el plugin
   nameserver   = "8.8.8.8"
-  searchdomain = "local.lan"
   ipconfig0    = "ip=192.168.10.${223 + count.index}/24,gw=192.168.10.1"
   
   sshkeys = <<EOF
